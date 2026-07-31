bbb6a0ba11e2d9039d6af1dc90ade75adf3ca38b
jasnok <jso46034@naver.com>
2026-07-31 14:02:17 +0900

    260731_fourth

--- frontend/core/auth.py
+++ frontend/core/auth.py
@@ -13,26 +13,43 @@ def init_state(
     st.session_state.setdefault("login_id", stored_login_id)
     st.session_state.setdefault("login_name", stored_login_name)
 
-
+# Login with Fronend
 def login(login_id:str, login_pwd:str) -> None:
-    try:
-        result = login_process(login_id, login_pwd)
-        if result is not None:
-            st.session_state.loginout = "login"
-            st.session_state.login_id = login_id
-            st.session_state.login_name = result["name"]
-            st.rerun()
-    except BackendAPIError as error: 
-        st.error(str(error))
 
+    if login_id == "id01" and login_pwd == "pwd01":
+        st.session_state.loginout = "login"
+        st.session_state.login_id = login_id
+        st.session_state.login_name = "이말숙"
+        st.rerun()
+    else:
+        st.error("로그인 아이디 또는 패스워드 틀림")
 
 def logout() -> None:
-    result = logout_process(st.session_state.login_id)
-    if result is not None:
-        st.session_state.loginout = "logout"
-        st.session_state.login_id = ""
-        st.session_state.login_pwd = ""
-        st.session_state.login_name = ""
+    st.session_state.loginout = "logout"
+    st.session_state.login_id = ""
+    st.session_state.login_pwd = ""
+    st.session_state.login_name = ""
+
+# Login with Backend
+# def login(login_id:str, login_pwd:str) -> None:
+#     try:
+#         result = login_process(login_id, login_pwd)
+#         if result is not None:
+#             st.session_state.loginout = "login"
+#             st.session_state.login_id = login_id
+#             st.session_state.login_name = result["name"]
+#             st.rerun()
+#     except BackendAPIError as error: 
+#         st.error(str(error))
+
+
+# def logout() -> None:
+#     result = logout_process(st.session_state.login_id)
+#     if result is not None:
+#         st.session_state.loginout = "logout"
+#         st.session_state.login_id = ""
+#         st.session_state.login_pwd = ""
+#         st.session_state.login_name = ""
 
 
 def is_logged_in() -> bool:
